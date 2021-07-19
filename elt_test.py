f7506698525819f99db3ad61ccac36532b2a7e08
YUNJINCHOI <p90m04@emart.com>
2021-07-19 11:04:10 +0900

    a

--- elt_test.py
+++ elt_test.py
@@ -2,10 +2,8 @@ from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 import pendulum
 from datetime import datetime
-from utils.alert import SlackAlert
 from airflow.utils.trigger_rule import TriggerRule
 
-alert = SlackAlert()
 KST = pendulum.timezone("Asia/Seoul")
 
 args = {
@@ -14,22 +12,22 @@ args = {
 #    'on_failure_callback' : alert.slack_fail_alert,
      }
 
-dag  = DAG(dag_id='elt_test2',
+dag  = DAG(dag_id='elt_test',
            default_args=args,
            schedule_interval='00 06 * * *')
 
 t1 = BashOperator(task_id='test_task1',
-                  bash_command='gcloud compute ssh "fast-elt-001" --zone "asia-northeast3-a" --project "smart-ruler-304409" --command "ls"',
+                  bash_command='ls',
                   trigger_rule=TriggerRule.ALL_DONE,
                   dag=dag)
 
 t2 = BashOperator(task_id='test_task2',
-                  bash_command='gcloud compute ssh "fast-elt-001" --zone "asia-northeast3-a" --project "smart-ruler-304409" --command "le"',
+                  bash_command='le',
                   trigger_rule=TriggerRule.ALL_DONE,
                   dag=dag)
 
 t3 = BashOperator(task_id='test_task3',
-                  bash_command='gcloud compute ssh "fast-elt-001" --zone "asia-northeast3-a" --project "smart-ruler-304409" --command "ls"',
+                  bash_command='ls',
                   trigger_rule=TriggerRule.ALL_DONE,
                   dag=dag)
 
