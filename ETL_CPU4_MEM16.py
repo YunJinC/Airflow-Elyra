26bccb90348728ce51c01a05e23ddd1e1dc120da
Choi YunJin <yunjinchoi@shinsegae.com>
2022-06-20 10:07:31 +0000

    T

--- ETL_CPU4_MEM16.py
+++ ETL_CPU4_MEM16.py
@@ -31,38 +31,6 @@ t1 = KubernetesPodOperator(
     image='perl',
     cmds=['perl'],
     arguments=['-Mbignum=bpi', '-wle', 'print bpi(2000)'],
-    # affinity allows you to constrain which nodes your pod is eligible to
-    # be scheduled on, based on labels on the node. In this case, if the
-    # label 'cloud.google.com/gke-nodepool' with value
-    # 'nodepool-label-value' or 'nodepool-label-value2' is not found on any
-    # nodes, it will fail to schedule.
-    affinity={
-        'nodeAffinity': {
-            # requiredDuringSchedulingIgnoredDuringExecution means in order
-            # for a pod to be scheduled on a node, the node must have the
-            # specified labels. However, if labels on a node change at
-            # runtime such that the affinity rules on a pod are no longer
-            # met, the pod will still continue to run on the node.
-            'requiredDuringSchedulingIgnoredDuringExecution': {
-                'nodeSelectorTerms': [{
-                    'matchExpressions': [{
-                        # When nodepools are created in Google Kubernetes
-                        # Engine, the nodes inside of that nodepool are
-                        # automatically assigned the label
-                        # 'cloud.google.com/gke-nodepool' with the value of
-                        # the nodepool's name.
-                        'key': 'cloud.google.com/gke-nodepool',
-                        'operator': 'In',
-                        # The label key's value that pods can be scheduled
-                        # on.
-                        'values': [
-                            'user-pool-cpu4-mem16',
-                        ]
-                    }]
-                }]
-            }
-        }
-    },
     dag=dag
     )
 
