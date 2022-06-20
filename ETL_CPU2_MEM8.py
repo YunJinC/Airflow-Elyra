fa563d2d42f9dd6b0d9bdf651df213cd4fff7814
Choi YunJin <yunjinchoi@shinsegae.com>
2022-06-20 09:11:57 +0000

    dd

---
+++ ETL_CPU2_MEM8.py
@@ -0,0 +1,80 @@
+from airflow.models import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.bash_operator import BashOperator
+import pendulum
+from datetime import datetime
+from datetime import timedelta
+from airflow.utils.trigger_rule import TriggerRule
+
+KST = pendulum.timezone("Asia/Seoul")
+
+args = {
+    'owner': 'airflow', 
+    'start_date': days_ago(1),
+#    'on_failure_callback' : alert.slack_fail_alert,
+     }
+
+dag  = DAG(dag_id='ETL_CPU2_MEM8',
+           default_args=args,
+           schedule_interval=timedelta(days=1),
+           tags=['Kube'],
+        )
+
+# Pod affinity with the KubernetesPodOperator
+# is not supported with Composer 2
+# instead, create a cluster and use the GKEStartPodOperator
+# https://cloud.google.com/composer/docs/using-gke-operator
+kubernetes_affinity_ex = KubernetesPodOperator(
+    task_id='ex-pod-affinity',
+    name='ex-pod-affinity',
+    namespace='default',
+    image='perl',
+    cmds=['perl'],
+    arguments=['-Mbignum=bpi', '-wle', 'print bpi(2000)'],
+    # affinity allows you to constrain which nodes your pod is eligible to
+    # be scheduled on, based on labels on the node. In this case, if the
+    # label 'cloud.google.com/gke-nodepool' with value
+    # 'nodepool-label-value' or 'nodepool-label-value2' is not found on any
+    # nodes, it will fail to schedule.
+    affinity={
+        'nodeAffinity': {
+            # requiredDuringSchedulingIgnoredDuringExecution means in order
+            # for a pod to be scheduled on a node, the node must have the
+            # specified labels. However, if labels on a node change at
+            # runtime such that the affinity rules on a pod are no longer
+            # met, the pod will still continue to run on the node.
+            'requiredDuringSchedulingIgnoredDuringExecution': {
+                'nodeSelectorTerms': [{
+                    'matchExpressions': [{
+                        # When nodepools are created in Google Kubernetes
+                        # Engine, the nodes inside of that nodepool are
+                        # automatically assigned the label
+                        # 'cloud.google.com/gke-nodepool' with the value of
+                        # the nodepool's name.
+                        'key': 'CPU',
+                        'operator': 'In',
+                        # The label key's value that pods can be scheduled
+                        # on.
+                        'values': [
+                            '2',
+                        ]
+                    },{
+                        # When nodepools are created in Google Kubernetes
+                        # Engine, the nodes inside of that nodepool are
+                        # automatically assigned the label
+                        # 'cloud.google.com/gke-nodepool' with the value of
+                        # the nodepool's name.
+                        'key': 'MEM',
+                        'operator': 'In',
+                        # The label key's value that pods can be scheduled
+                        # on.
+                        'values': [
+                            '8',
+                        ]
+                    }]
+                }]
+            }
+        }
+    })
+
+t1
---
+++ ETL_ON_PREM.py
@@ -0,0 +1,28 @@
+from airflow.models import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.bash_operator import BashOperator
+import pendulum
+from datetime import datetime
+from datetime import timedelta
+from airflow.utils.trigger_rule import TriggerRule
+
+KST = pendulum.timezone("Asia/Seoul")
+
+args = {
+    'owner': 'airflow', 
+    'start_date': days_ago(1),
+#    'on_failure_callback' : alert.slack_fail_alert,
+     }
+
+dag  = DAG(dag_id='ETL_ON_PREM',
+           default_args=args,
+           schedule_interval=timedelta(days=1),
+           tags=['Kube'],
+        )
+
+t1 = BashOperator(task_id='task1',
+                  bash_command='ls',
+                  trigger_rule=TriggerRule.ALL_DONE,
+                  dag=dag)
+
+t1
