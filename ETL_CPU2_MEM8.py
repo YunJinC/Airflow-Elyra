1246951ddc7e3424b7d576301ae6f72bd21fa00e
Choi YunJin <yunjinchoi@shinsegae.com>
2022-06-21 10:55:01 +0000

    y

--- ETL_CPU2_MEM8.py
+++ ETL_CPU2_MEM8.py
@@ -1,10 +1,12 @@
 from airflow.models import DAG
 from airflow.utils.dates import days_ago
-from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 import pendulum
 from datetime import datetime
 from datetime import timedelta
 from airflow.utils.trigger_rule import TriggerRule
+from kubernetes.client import models as k8s
+
 
 KST = pendulum.timezone("Asia/Seoul")
 
@@ -20,9 +22,35 @@ dag  = DAG(dag_id='ETL_CPU2_MEM8',
            tags=['Kube'],
         )
 
-t1 = BashOperator(task_id='task1',
-                  bash_command=f'kubectl apply -f kubernetes/ETL_CPU2_MEM8.yaml',
-                  trigger_rule=TriggerRule.ALL_DONE,
-                  dag=dag)
+affinity = k8s.V1Affinity(
+            node_affinity=k8s.V1NodeAffinity(
+                required_during_scheduling_ignored_during_execution=k8s.V1NodeSelector(
+                    node_selector_terms = [
+                        k8s.V1NodeSelectorTerm(
+                            match_expressions=[
+                                k8s.V1NodeSelectorRequirement(key='CPU', operator='In', values=['2'])
+                            ]
+                        )
+                    ]
+                )
+            )
+        )
+# Pod affinity with the KubernetesPodOperator
+# is not supported with Composer 2
+# instead, create a cluster and use the GKEStartPodOperator
+# https://cloud.google.com/composer/docs/using-gke-operator
+t1 = KubernetesPodOperator(
+    task_id='ETL_CPU2_MEM8_TASK',
+    name='ETL_CPU2_MEM8_TASK',
+    # namespace='default',
+    namespace='airflow-cluster',
+    image='perl',
+    cmds=['perl'],
+    arguments=['-Mbignum=bpi', '-wle', 'print bpi(2000)'],
+    # is_delete_operator_pod=True,
+    # in_cluster=True,
+    # affinity=affinity,
+    dag=dag
+    )
 
 t1
